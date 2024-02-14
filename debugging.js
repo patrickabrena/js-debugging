c7e8d6bd2d43ae131c7e1591fd94fac4155aa358
Patrick Abrena <patrickabrena@hotmail.com>
2024-02-14 12:31:24 -0600

    finish debugging section. Everything else I've already done while building projects

---
+++ debugging.js
@@ -0,0 +1,12 @@
+const USE_TYPEOF_TO_CHECK_THE_TYPE_OF_A_VARIABLE = () => {
+  let seven = 7;
+  let three = "3";
+  console.log(seven + three);
+  // Only change code below this line
+  console.log(typeof seven);
+  console.log(typeof three);
+};
+USE_TYPEOF_TO_CHECK_THE_TYPE_OF_A_VARIABLE();
+
+// just look at freeCodeCamp's debugging section under legacy JS DS&A course
+// everything else about catching bugs is stuff I've already done by just building projects
